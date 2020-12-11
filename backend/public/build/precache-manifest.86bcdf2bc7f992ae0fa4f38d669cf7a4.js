edc0ab9fc99fa39ebfc3947e61c82493f1465828
Isabel Ferrara <ifm95@hotmail.com>
2020-12-11 13:03:58 -0600

    deploy

---
+++ backend/public/build/precache-manifest.86bcdf2bc7f992ae0fa4f38d669cf7a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7385451b1489367815df0e55425b5dca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c126fa9725d2cd2ecbf8",
+    "url": "/static/css/2.9d6f32cf.chunk.css"
+  },
+  {
+    "revision": "e3ffb7301fbffa30cbaf",
+    "url": "/static/css/main.cd4750db.chunk.css"
+  },
+  {
+    "revision": "c126fa9725d2cd2ecbf8",
+    "url": "/static/js/2.ed551b0b.chunk.js"
+  },
+  {
+    "revision": "e3ffb7301fbffa30cbaf",
+    "url": "/static/js/main.5cb27080.chunk.js"
+  },
+  {
+    "revision": "fb47f3c02db3ce888eb4",
+    "url": "/static/js/runtime-main.dc128e9d.js"
+  }
+]);
